1b879bd3acd354306662d91639c99d470d3398be
avdhoot <avdhoothadke@gmail.com>
2025-12-02 21:13:12 +0530

    task routes created

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { createTask, deleteTask, getTasks, updateTask } from "../controllers/taskcontroller.js";
+
+export const router = express.Router();
+
+router.route('/')
+    .get(getTasks)
+    .post(createTask);
+
+router.route('/:id')
+    .put(updateTask)
+    .delete(deleteTask);
+
